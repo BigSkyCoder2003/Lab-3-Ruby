46383379069086c9f18f0891137d896016f7aa98
Daniel Lounsbury <lounsbud@oregonstate.edu>
2024-02-15 13:36:27 -0800

    Finish Scissors.rb compare_to

--- Scissors.rb
+++ Scissors.rb
@@ -1,6 +1,33 @@
 require_relative:'element.rb'
 class Scissors < Element
   def compare_to(element)
+    case element.name
+
+    when self.name
+      return "#{element.name} equals #{self.name}"
     
+    when 'Lizard'
+      return "#{self.name} decapitates #{element.name}"
+
+    when 'Paper' 
+      return "#{self.name} cut #{element.name}"
+
+    when 'Rock'
+      return "#{element.name} crushes #{self.name}"
+    when 'Spock'
+      return "#{element.name} smashes #{self.name}"
+    else
+      fail 'error comparing elements!'
+  end
   end
 end
+# Scissors cut Paper
+# Paper covers Rock
+# Rock crushes Lizard
+# Lizard poisons Spock
+# Spock smashes Scissors
+# Scissors decapitate Lizard
+# Lizard eats Paper
+# Paper disproves Spock
+# Spock vaporizes Rock
+# Rock crushes Scissors
